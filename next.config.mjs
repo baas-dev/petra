195431ddcee8a48c598b24ff5beafbf15ee9f2f9
Tom Miller <wthomasmiii@gmail.com>
2023-10-18 22:28:50 -0500

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -2,25 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
 
-  async redirects() {
-    return [
-      {
-        source: "/backend/:path*",
-        destination: "http://127.0.0.1:4000/:path*",
-        permanent: false,
-      },
-    ]
-  },
-  async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          source: "/backend/:path*",
-          destination: "http://127.0.0.1:4000/:path*",
-        },
-      ],
-    }
-  },
   images: {
     domains: ["*"],
     remotePatterns: [
