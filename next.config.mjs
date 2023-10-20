68b1801d3426fbb177aca322c600416855ffa5d8
Tom Miller <wthomasmiii@gmail.com>
2023-10-19 20:16:23 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,25 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination: `http://${process.env.NEXT_PUBLIC_APIURL}/:path*`,
+        permanent: false,
+      },
+    ]
+  },
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/backend/:path*",
+          destination: `http://${process.env.NEXT_PUBLIC_APIURL}/:path*`,
+        },
+      ],
+    }
+  },
   images: {
     domains: ["*"],
     remotePatterns: [
