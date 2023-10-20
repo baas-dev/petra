1ed1593ad04b9afe17b2e7481c2c222a384c6116
Tom Miller <wthomasmiii@gmail.com>
2023-10-19 20:23:17 -0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     return [
       {
         source: "/backend/:path*",
-        destination: `http://${process.env.NEXT_PUBLIC_APIURL}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_APIURL}/:path*`,
         permanent: false,
       },
     ]
@@ -16,7 +16,7 @@ const nextConfig = {
       beforeFiles: [
         {
           source: "/backend/:path*",
-          destination: `http://${process.env.NEXT_PUBLIC_APIURL}/:path*`,
+          destination: `${process.env.NEXT_PUBLIC_APIURL}/:path*`,
         },
       ],
     }
